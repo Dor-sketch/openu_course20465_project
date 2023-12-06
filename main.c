4a05b09f3edfd0e702f6610065b295ce72f612ee
Dor-sketch <dor.pascal@mail.huji.ac.il>
2023-12-06 18:25:58 +0200

    Refactor and enhance error handling in main.c
    - Introduced process_assembly_file for improved modularity in assemble function.
    - Enhanced error checking in update_input_filename and throughout assembly process.
    - Streamlined resource management with free_resources function.
    - Improved readability and maintainability of code.

--- main.c
+++ main.c
@@ -1,92 +1,121 @@
-/*
-	assembler: The program transfers assembly language files into a machine code
-	it gets  files' names from the command line (without the ".as" extension),
-	if the file "name.as" includes valid code statments and macros, it creates
-	the following new files:
-    "name.am"        for the source without macros abbrevetions
-    "name.object"    for the machine code
-    "name.externals" for external data words information
-    "name.enrties"   for the "entry" type sympols in the file
-*/
-
 #include "syntax.h"
 #include "first_pass.h"
-#include "second_pass.h"
-#include "print_output.h"
 #include "pre.h"
+#include "print_output.h"
+#include "second_pass.h"
 
-#define MAX_CODE_ARR 600 /* maximum array size for code and data images */
+#define MAX_CODE_ARR 600 /* Maximum array size for code and data images */
 
-extern char *strdup(const char*);
+extern char *strdup(const char *);
 
-/* free_machine_img: a function to free the memmory allocated for machine words
-	during the previous file's assembling */
-void free_machine_img(machine_word **img, int start, int counter)
-{
-    int i;
-    
-    for (i = start; (i < counter) && (img[i] != NULL);) {
-        free(img[i]);
-        i++;
+/* Frees memory allocated for machine words in an image array */
+void free_machine_img(machine_word **img, int start, int counter) {
+    for (int i = start; i < counter && img[i] != NULL; i++) {
+        if (img[i] != NULL) {
+            free(img[i]);
+        }
     }
 }
 
-/* assemble: the function gets a file name and assembles its machine code as
-	described above. First it breakes MACRO lines into their content, written as
-	a new ".am" file, then it reads the file twice while assembling the code and
-	data images, using the functions from the first and second passes' files.
-	If no errors found it prints the results using the funcions from the "print
-	output" file. */ 
-void assemble(char *argv)
-{
-    machine_word *code_img[MAX_CODE_ARR]; 
-    machine_word *data_img[MAX_CODE_ARR];
-    /* code and data images made of poiners to "machine word" structure */
-    int ic, dc; /* an Instructions-Counter and a Data-Counter variables */
-    FILE *am_file; /* a file pointer for the file after macros expanding */
-    char *as_filename; /* for the ".as" file extension */
-    int *error_flag;
+/* Frees various resources allocated during the assembly process */
+void free_resources(char *as_filename, int *error_flag, machine_word **code_img,
+                    machine_word **data_img, int ic, int dc) {
+    if (as_filename != NULL) {
+        free(as_filename);
+    }
+    if (code_img != NULL) {
+        free_machine_img(code_img, 100, ic);
+    }
+    if (data_img != NULL) {
+        free_machine_img(data_img, 0, dc);
+    }
+}
 
-    ic  = 100; /* first 100 places saved for the machine */
-    dc  = 0;         
-    as_filename = (char *)malloc(strlen(argv) + 4); /* +4 for file extention and null terminator*/
-    error_flag = (int *)malloc(sizeof(int));
-    strcpy(as_filename, argv);
-    strcat(as_filename, ".as");
+/* Updates and returns a new filename with '.as' extension */
+char *update_input_filename(char *filename) {
+    char *new_filename = malloc(strlen(filename) + 4);
+    if (new_filename == NULL) {
+        return NULL;
+    }
+    strcpy(new_filename, filename);
+    strcat(new_filename, ".as");
+    return new_filename;
+}
 
-    expand_macros(as_filename, &(am_file));
-    
-    as_filename[strlen(as_filename) - 1] = 'm';/*now has ".am" at string's end*/
-    /* run first pass */
-    dc = get_first_img(ic, dc, code_img, data_img, am_file, as_filename,
-    				   error_flag);     
-    
-    fseek(am_file, 0, SEEK_SET); /* reset the pointer and start second pass */
-    ic = get_second_img(code_img, data_img, am_file, as_filename, error_flag);
+/* Handles the first pass of assembly process */
+int generate_first_pass(char *as_filename, FILE **ex_src_fl, int *error_flag,
+                        int *ic, int *dc, machine_word **code_img, machine_word **data_img) {
+    as_filename[strlen(as_filename) - 1] = 'm'; /* Change to ".am" */
+    printf("Running first pass with ic = %d, dc = %d\n", *ic, *dc);
+    int updated_dc = get_first_img(*ic, *dc, code_img, data_img, *ex_src_fl, as_filename, error_flag);
+    return updated_dc;
+}
+
+/* Checks and reports if an error occurred during assembly */
+int check_error_flag(int *error_flag) {
+    if (*error_flag == EXIT_FAILURE) {
+        printf("Error: File has errors, no output files created.\n");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+/* Processes a single assembly file */
+void process_assembly_file(char *as_filename, int *ic, int *dc,
+                           machine_word **code_img, machine_word **data_img,
+                           int *error_flag) {
+    FILE *am_file = NULL;
+
+    expand_macros(as_filename, &am_file);
+    if (!am_file) {
+        *error_flag = EXIT_FAILURE;
+        return;
+    }
+
+    *dc = get_first_img(*ic, *dc, code_img, data_img, am_file, as_filename, error_flag);
+    if (*error_flag != EXIT_SUCCESS) {
+        fclose(am_file);
+        return;
+    }
+
+    fseek(am_file, 0, SEEK_SET); // Reset for second pass
+    *ic = get_second_img(code_img, data_img, am_file, as_filename, error_flag);
     fclose(am_file);
-    if (*error_flag == EXIT_SUCCESS) {
+}
+
+/* Handles the assembly process */
+void assemble(char *argv) {
+    machine_word *code_img[MAX_CODE_ARR] = {0};
+    machine_word *data_img[MAX_CODE_ARR] = {0};
+    int ic = 100, dc = 0;
+    char *as_filename = update_input_filename(argv);
+
+
+    if (!as_filename) {
+        fprintf(stderr, "Error: Failed to update input filename.\n");
+        return;
+    }
+
+    int error_flag = EXIT_SUCCESS;
+    process_assembly_file(as_filename, &ic, &dc, code_img, data_img, &error_flag);
+
+    if (check_error_flag(&error_flag) == EXIT_SUCCESS) {
         make_output(code_img, data_img, ic, dc, as_filename);
     }
-    free(as_filename);
-    free(error_flag);
-    free_machine_img(code_img, 100, ic);
-    free_machine_img(data_img, 0, dc);
-    return;
+
+    free_resources(as_filename, &error_flag, code_img, data_img, ic, dc);
+    printf("Finished assembling %s\n", argv);
 }
 
-/*  main: the main function gets ".as" file names from the command line 
-	(withot	file's extentions ".as"), and assemble each file - if valid. */
-int main(int argc, char *argv[])
-{
-	int files_to_assamble;
-    int i;
-	
-	if ((files_to_assamble = argc) <= 1) {/* if no file names are given */
-		printf("error: program needs at least one file to assemble.\n");
-	}
-	
-    for (i = 1; i < files_to_assamble; i++) {
-       assemble(*(argv +i));
+/* Main function: processes each file passed as a command-line argument */
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        printf("Error: Program needs at least one file to assemble.\n");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        assemble(argv[i]);
     }
 
     return EXIT_SUCCESS;
